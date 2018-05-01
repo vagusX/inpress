3616408b7d98d82f4a225eb928a7cf76128f2c88
vagusX <vagusxl@gmail.com>
2018-05-01 22:08:22 +0800

    feat: first implement for dev cmd

---
+++ next.config.js
@@ -0,0 +1,21 @@
+const images = require('remark-images')
+
+module.exports = {
+  pagesExtensions: ['js', 'jsx', 'md', 'mdx'],
+  webpack: (config, { defaultLoaders }) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: [
+        defaultLoaders.babel,
+        {
+          loader: '@mdx-js/loader',
+          options: {
+            mdPlugins: [images]
+          }
+        }
+      ]
+    })
+
+    return config
+  }
+}
